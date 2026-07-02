2da98ac050685292fc5c39b38c26186c30efeb22
Anointed-wheels <toluleyeoluwaseyi@gmail.com>
2026-07-02 05:46:51 -0100

    push

--- library_system/settings.py
+++ library_system/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hotwheelscoder.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
